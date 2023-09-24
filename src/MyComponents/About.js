b2c38e543b9abcbb6b438117c234e5aba4213ecf
Tejas Rajput <88982671+Tejas-Rajput@users.noreply.github.com>
2023-09-24 20:55:37 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, mollitia recusandae ex nobis quis hic, officiis asperiores aperiam eaque laborum quaerat est impedit exercitationem ea? Fugiat officiis hic provident excepturi?
+      </p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  let footerStyle = {
+    position: "relative",
+    width: "100%"
+  }
+  return (
+    <footer className='bg-dark text-light py-3' style={footerStyle}>
+      <p className='text-center'>
+        Copyright &copy; My TodosList.com
+      </p>
+    </footer>
+  )
+}
